c51538443342e04873c3dc76ad91acb2d6f9e04a
“lbyul” <rlaquf7737@naver.com>
2024-08-22 01:44:20 +0900

    시간 표시

--- 001/jsx/src/App.js
+++ 001/jsx/src/App.js
@@ -1,7 +1,15 @@
 function App() {
+
+  const time = new Date();
+
+
   return (
     <div>
-      hello world
+      <div style={{backgroundColor: 'pink', color: 'green', fontSize:'24px', fontWeight: 'bold'}}>
+        <p>년: {time.getFullYear()}</p>
+        <p>월/일: {time.getMonth() + 1}/{time.getDate()}</p>
+        <p>시간: {time.getHours()}시 {time.getMinutes()}분 {time.getSeconds()}초</p>
+      </div>
     </div>
   );
 }
